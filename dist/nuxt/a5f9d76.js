2119884694a30cc1c4ef81a42c7dc34343c2df3f
dreamableca <77071409+dreamableca@users.noreply.github.com>
2022-11-28 10:15:44 -0800

    Build To Test 1.2 (4)

---
+++ dist/nuxt/a5f9d76.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[186],{1050:function(n,e,r){"use strict";r.r(e),r.d(e,"SmsManagerPluginWeb",(function(){return o}));var t=r(54);class o extends t.b{constructor(){super({name:"SmsManager",platforms:["web"]}),this.NOT_SUPPORTED="ERR_PLATFORM_NOT_SUPPORTED"}async send(n){return new Promise(((n,e)=>{e(this.NOT_SUPPORTED)}))}}}}]);
