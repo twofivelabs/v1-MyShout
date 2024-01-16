dd00a753e787b41cbc32281e434c39e22fa75bac
kyleperison <kyle@perison.ca>
2024-01-16 09:24:00 -0800

    Chat Settings Full Screen

--- components/Chat/Actionsbtn.vue
+++ components/Chat/Actionsbtn.vue
@@ -3,10 +3,14 @@
     <v-icon class="pa-3 rounded-lg" color="myshoutDarkGrey" @click="showBottomSheet = true">
       mdi-dots-vertical
     </v-icon>
-    <v-bottom-sheet v-model="showBottomSheet" :scrollable="true" max-width="700">
-      <v-sheet height="95vh" class="rounded-t-xl">
+    <v-bottom-sheet v-model="showBottomSheet" :scrollable="true" max-width="700" fullscreen>
+      <v-sheet>
         <div class="ma-3" style="padding-bottom:180px;">
-          <GlobalSlidebar v-touch="{ down: () => swipe('Down') }" @click.native="swipe('Down')" />
+          <div class="text-right">
+            <v-icon class="pa-3 rounded-lg" color="myshoutLightGrey" @click="showBottomSheet = false">
+              mdi-close-thick
+            </v-icon>
+          </div>
 
           <v-row no-gutters class="text-center mb-10">
             <v-col cols="12">
@@ -21,47 +25,10 @@
           </v-row>
             
 
-          <v-list-item-group v-if="participants">
-            Members
-            <v-list-item key="add-member">
-              <v-list-item-avatar>
-                <v-icon small>mdi-account-plus</v-icon>
-              </v-list-item-avatar>
-              <v-list-item-title>
-                Add Members
-              </v-list-item-title>
-            </v-list-item>
-            <v-list-item
-              v-for="(participant, index) in participants"
-              :key="index"
-            >
-              <v-list-item-avatar>
-                <ChatAvatar :user="participant" />
-              </v-list-item-avatar>
-              <v-list-item-title>
-                {{ participant.username }}
-              </v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
+          <ChatActionsMemberslist :chat="chat" :participants="participants" />
+          <ChatActionsSettingslist :chat="chat" :participants="participants" />
 
-          <v-list-item-group class="mt-10" v-if="chat && participants">
-            <v-list-item key="mute-chat" @click="setChatMuteState()">
-              <v-list-item-avatar>
-                <v-icon small>mdi-volume-off</v-icon>
-              </v-list-item-avatar>
-              <v-list-item-title>
-                {{ !chat.muted.includes(user.data.uid) ? 'Mute Notifications' : 'Unmute Notification'}}
-              </v-list-item-title>
-            </v-list-item>
-            <v-list-item key="leave-group">
-              <v-list-item-avatar>
-                <v-icon small color="red">mdi-exit-to-app</v-icon>
-              </v-list-item-avatar>
-              <v-list-item-title class="red--text">
-                {{ participants.size > 1 ? 'Leave Group' : 'Leave Chat' }}
-              </v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
+          
             <!--
               <ChatActionsViewmembersbtn :chat="chat" :participants="participants" />
               <ChatActionsRenamebtn :chatId="chat.id" />
@@ -83,8 +50,6 @@ import {
 } from '@nuxtjs/composition-api'
 
 import { Touch } from 'vuetify/lib/directives'
-import firebase from 'firebase';
-import 'firebase/functions';
 
 export default defineComponent({
   name: 'ChatActionsbtn',
@@ -103,8 +68,8 @@ export default defineComponent({
       }
     }
   },
-  setup(props) {
-    const { state, dispatch } = useStore()
+  setup() {
+    const { state } = useStore()
     const user = computed(() => state.user);
     
     const showBottomSheet = ref(false)
@@ -115,18 +80,10 @@ export default defineComponent({
       }
     }
 
-    const setChatMuteState = async () => {
-      console.log(`Mute Button Selected For User ${user.value.data.uid}`, props.chat.muted)
-      return await dispatch('chats/updateField', {
-        id: props.chat.id,
-        muted: !props.chat.muted.includes(user.value.data.uid) ? firebase.firestore.FieldValue.arrayUnion(user.value.data.uid) : firebase.firestore.FieldValue.arrayRemove(user.value.data.uid)
-      })
-    }
-
     return {
-      showBottomSheet,
       user,
-      swipe, setChatMuteState
+      showBottomSheet,
+      swipe,
     }
   }
 })
